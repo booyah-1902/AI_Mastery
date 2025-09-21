077a010cb4e765084d740eed49242449c73de208
Ayyub <rayaanmoghul@gmail.com>
2025-09-22 00:30:29 +0100

    Game Logic Pt.2 Completed

--- AI-Mastery-Week-1/war_card_game.py
+++ AI-Mastery-Week-1/war_card_game.py
@@ -1,4 +1,4 @@
-import random
+from random import shuffle
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
@@ -28,3 +28,60 @@ class Deck:
         
     def shuffle(self):
         
+        shuffle(self.all_cards)
+
+    def deal_one(self):
+
+        return self.all_cards.pop()
+    
+class Player:
+
+    def __init__(self, name):
+        
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+
+    def __str__ (self):
+        return (f"Player {self.name} has {len(self.all_cards)} cards.")
+
+
+def main():
+    game_on = True
+    round_num = 0
+    player_one = Player("One")
+    player_two = Player("Two")
+
+    new_deck = Deck()
+    new_deck.shuffle()
+
+    for x in range(26):
+        player_one.add_cards(new_deck.deal_one())
+        player_two.add_cards(new_deck.deal_one())
+    
+    while game_on:
+        round_num += 1
+        print(f"Round {round_num}")
+
+        if len(player_one.all_cards) == 0:
+            print("Player One, out of cards! Player Two Wins!")
+            game_on = False
+            break
+            
+        if len(player_two.all_cards) == 0:
+            print("Player Two, out of cards! Player One Wins!")
+            game_on = False
+            break
+
+        player_one_cards = []
+        player_one_cards.append(player_one.remove_one())
+        player_two_cards = []
+        player_two_cards.append(player_two.remove_one())
